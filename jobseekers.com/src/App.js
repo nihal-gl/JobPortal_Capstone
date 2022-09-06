0ea96112668d06bb80d9fb6cae111108fb54b166
wdevrachit <rachit.tandon@globallogic.com>
2022-09-06 14:37:15 +0530

    update

--- jobseekers.com/src/App.js
+++ jobseekers.com/src/App.js
@@ -1,5 +1,10 @@
 import TestComponent from './TestComponent';
+
 import Login from './login';
+import PranjalDummyComp from './PranjalDummyComp';
+import TestComponentNihal from './TestComponentNihal'
+import Shashwatcomponent from './ShashwatComponent/Shashwatcomponent';
+import TestRachit from './TestRachit';
 
 function App() {
   return (
@@ -7,6 +12,10 @@ function App() {
       {/* RENDER YOU COMPONENT HERE */}
       <TestComponent></TestComponent>
       <Login></Login>
+      <PranjalDummyComp></PranjalDummyComp>
+      <TestComponentNihal></TestComponentNihal>
+      <Shashwatcomponent></Shashwatcomponent>
+      <TestRachit></TestRachit>
     </div>
   );
 }
