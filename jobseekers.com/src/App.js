7d6c86a0f6f62baadb345f103f74ba81dcd3f603
nihal-gl <nihal.pandey@globallogic.com>
2022-09-07 15:04:35 +0530

    linked firebase and user form

--- jobseekers.com/src/App.js
+++ jobseekers.com/src/App.js
@@ -1,10 +1,11 @@
-
+import ProfileForm from "./Components/ProfileForm";
 
 function App() {
   return (
     <div className="App">
       {/* RENDER YOU COMPONENT HERE */}
       <h1>Go get a job !</h1>
+      <ProfileForm/>
     </div>
   );
 }
