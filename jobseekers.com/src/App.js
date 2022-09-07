d5137ad0a74cf54316469004f369caff160c333c
Pranjal Tripathi <pranjal.tripathi@globallogic.com>
2022-09-07 16:38:00 +0530

    added font awesome icon

--- jobseekers.com/src/App.js
+++ jobseekers.com/src/App.js
@@ -1,8 +1,11 @@
+import JobsListing from "./Components/JobsListing";
+
 function App() {
   return (
     <div className="App">
       {/* RENDER YOU COMPONENT HERE */}
       <h1>Go get a job !</h1>
+      <JobsListing></JobsListing>
     </div>
   );
 }
