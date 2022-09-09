302d7289f640f26769feaff9adfebebf3822b6a2
Pranjal Tripathi <pranjal.tripathi@globallogic.com>
2022-09-09 09:29:15 +0530

    added firebase config

--- jobseekers.com/src/firebase/config.js
+++ jobseekers.com/src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC6AvEBkQXfFFankHWA_3uJitOe_miCydc",
-    authDomain: "gl-job-app.firebaseapp.com",
-    projectId: "gl-job-app",
-    storageBucket: "gl-job-app.appspot.com",
-    messagingSenderId: "670750833078",
-    appId: "1:670750833078:web:0a68ad8e6724235f4ce917"
-  };
+  apiKey: "AIzaSyAf45W5HydEAsuW4Tmd9g7KSGBO2XPdgwM",
+  authDomain: "job-seeker-d56c3.firebaseapp.com",
+  projectId: "job-seeker-d56c3",
+  storageBucket: "job-seeker-d56c3.appspot.com",
+  messagingSenderId: "729872060795",
+  appId: "1:729872060795:web:4f3a9f7d872cf315a5adde"
+};
 
   firebase.initializeApp(firebaseConfig)
 
