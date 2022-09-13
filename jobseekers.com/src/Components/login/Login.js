3f185f4daddbd3edd2934e2e3ad5cd36b5944642
Pranjal Tripathi <pranjal.tripathi@globallogic.com>
2022-09-13 13:06:49 +0530

    fixed route to admin

--- jobseekers.com/src/Components/login/Login.js
+++ jobseekers.com/src/Components/login/Login.js
@@ -97,8 +97,8 @@ const Login = () => {
                     dispatch(setUserRole({role: 'admin'}));
                     dispatch(setUserId({id: res.docs[0].id}))
                     console.log('navigate to admin pages');
-                    navigate('/jobadmin') // intended page to route (admin dashboard )
-                    // navigate('/adminpanel') // intended page to route (admin dashboard )
+                    // navigate('/jobadmin') // intended page to route (admin dashboard )
+                    navigate('/adminpanel') // intended page to route (admin dashboard )
                    
                 } else {
                     dispatch(setUserRole({role: 'user'}));
